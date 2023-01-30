f7e556514e9ca7f0f291f45cc0e4a0f465d1e48b
Justine <justine.telmann@gmail.com>
2023-01-30 15:33:02 +0100

    creation nouvelle branch

---
+++ src/Controller/StatistiqueController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StatistiqueController extends AbstractController
+{
+    #[Route('/statistique', name: 'app_statistique')]
+    public function index(): Response
+    {
+        return $this->render('statistique/index.html.twig', [
+            'controller_name' => 'StatistiqueController',
+        ]);
+    }
+}
